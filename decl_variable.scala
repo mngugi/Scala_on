9b103b38c4f8d7f3cf34997b3232f94c08569a63
M.ngugi <mngugi7@gmail.com>
2024-04-07 00:59:55 +0300

    Create decl_variable.scala

---
+++ decl_variable.scala
@@ -0,0 +1,11 @@
+object Varls {
+	def main(args: Array[String]) {
+		var variable_ : Int = 16
+		val variable_1 : String = "Scala looks interesting! more like JS "
+		
+		println(variable_); println(variable_1);
+		
+		
+		}
+	
+	}
